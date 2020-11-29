ba63d791018e4e90beeec9ee37f3dcae8842fbb2
Matt <lezgomatt@gmail.com>
2020-11-29 23:17:40 +0800

    tests: Add tests for `roundOff`

--- utils.test.js
+++ utils.test.js
@@ -1,4 +1,4 @@
-const { rotate } = require('./utils');
+const { rotate, roundOff } = require('./utils');
 
 test('rotate([1, 2, 3], 0) = [1, 2, 3]', () => {
   expect(rotate([1, 2, 3], 0)).toStrictEqual([1, 2, 3]);
@@ -18,4 +18,12 @@ test('rotate([1, 2, 3], 3) = [1, 2, 3]', () => {
 
 test('rotate([1, 2, 3], 4) = [2, 3, 1]', () => {
   expect(rotate([1, 2, 3], 4)).toStrictEqual([2, 3, 1]);
+});
+
+test('roundOff(123.45, 1) = 123.5', () => {
+  expect(roundOff(123.45, 1)).toBe(123.5);
+});
+
+test('roundOff(0.254, 2) = 0.25', () => {
+  expect(roundOff(0.254, 2)).toBe(0.25);
 });
