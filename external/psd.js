edec789b97ee07e333b3e2a0dffe107f562c093b
Matt <lezgomatt@gmail.com>
2020-12-17 22:43:24 +0800

    docs: Explain why we include a prebuilt psd.js

--- external/psd.js
+++ external/psd.js
@@ -1,3 +1,8 @@
+// Taken from: https://github.com/meltingice/psd.js/blob/master/dist/psd.js
+// Because of the structure of psd.js (an old CoffeeScript project),
+// it's not trivial to use the node package with modern frontend build tools.
+// So we use the prebuilt file from the repo instead (with a few modifications).
+
 R=(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({"./image_exports/png.coffee":[function(require,module,exports){
 var RSVP;
 
