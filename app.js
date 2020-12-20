000556ff260f8139db958d6f071b1434207a0c31
Matt <lezgomatt@gmail.com>
2020-12-20 15:11:54 +0800

    feature: Select file via browser

--- app.js
+++ app.js
@@ -1,9 +1,25 @@
 import PSD from 'psd';
 import { convertToSvg } from '.';
 
+let fileBrowser = document.getElementById('file-browser');
 let output = document.getElementById('output');
 let preview = document.getElementById('preview');
 let downloadLink = document.getElementById('download-link');
+
+fileBrowser.addEventListener('change', (event) => {
+    let file = fileBrowser.files[0];
+    let filename = file.name;
+
+    PSD.fromDroppedFile(file).then(function (psd) {
+        let svg = convertToSvg(psd);
+        let svgDataUrl = 'data:image/svg+xml,' + window.encodeURI(svg.toString());
+
+        preview.src = svgDataUrl;
+        downloadLink.href = svgDataUrl;
+        downloadLink.download = filename + '.svg';
+        output.hidden = false;
+    });
+});
 
 document.body.addEventListener('dragover', (event) => {
     event.preventDefault();
