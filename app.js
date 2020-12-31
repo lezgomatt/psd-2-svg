8c22d28a124107e97c7a18321498cfd36214104f
Matt <lezgomatt@gmail.com>
2020-12-31 23:35:00 +0800

    ui: Move upload button and remove blank previews

--- app.js
+++ app.js
@@ -1,17 +1,12 @@
 import PSD from 'psd';
 import { convertToSvg } from '.';
 
+let fileBrowser = document.querySelector('.file-browser');
 let outputContainer = document.getElementById('output-container');
 let template = document.getElementById('output-template');
 
-document.addEventListener('change', (event) => {
-    let fileBrowser = event.target.closest('.file-browser');
-    if (fileBrowser == null) {
-        return;
-    }
-
-    let output = fileBrowser.closest('.output');
-    convert(fileBrowser.files[0], output);
+fileBrowser.addEventListener('change', (event) => {
+    convert(fileBrowser.files[0]);
 });
 
 document.addEventListener('click', (event) => {
@@ -21,11 +16,7 @@ document.addEventListener('click', (event) => {
     }
 
     let output = preview.closest('.output');
-    if (output.dataset.converted) {
-        output.querySelector('.download-link').click();
-    } else {
-        output.querySelector('.file-browser').click();
-    }
+    output.querySelector('.download-link').click();
 });
 
 document.body.addEventListener('dragover', (event) => {
@@ -37,7 +28,7 @@ document.body.addEventListener('drop', (event) => {
     convert(event.dataTransfer.files[0], document.querySelector('.output'));
 });
 
-async function convert(file, output) {
+async function convert(file) {
     if (!file.name.endsWith('.psd')) {
         alert(`Expected a Photoshop document (.psd), got "${file.name}" instead.`);
         return;
@@ -47,20 +38,14 @@ async function convert(file, output) {
     let svg = convertToSvg(psd);
     let svgDataUrl = 'data:image/svg+xml,' + window.encodeURI(svg.toString());
 
+    let output = template.content.cloneNode(true);
+
     let preview = output.querySelector('.preview');
     let downloadLink = output.querySelector('.download-link');
 
     preview.src = svgDataUrl;
     downloadLink.href = svgDataUrl;
     downloadLink.download = file.name + '.svg';
-    output.dataset.converted = true;
 
-    addOutput();
+    outputContainer.appendChild(output);
 }
-
-function addOutput() {
-    let copy = template.content.cloneNode(true);
-    outputContainer.appendChild(copy);
-}
-
-addOutput();
