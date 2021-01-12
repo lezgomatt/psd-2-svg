67f365ea0cd95d44ba9d7d4406cd28c86d078f30
Matt <lezgomatt@gmail.com>
2021-01-12 22:30:04 +0800

    feature: Improve CLI

--- psd-2-svg.js
+++ psd-2-svg.js
@@ -1,5 +1,23 @@
 #!/usr/bin/env node
 
+const fs = require('fs');
 const { convertFile } = require('.');
 
-console.log(convertFile(process.argv[2]).toString());
+const usage = 'Usage: psd-2-svg <psd-file> <svg-output>';
+
+if (process.argv.length !== 4) {
+    return console.error(usage);
+}
+
+let psdPath = process.argv[2];
+if (!fs.existsSync(psdPath)) {
+    return console.error('File not found: ' + psdPath);
+}
+
+let svgPath = process.argv[3];
+if (fs.existsSync(svgPath)) {
+    return console.error('File already exists: ' + svgPath);
+}
+
+let output = convertFile(psdPath).toString();
+fs.writeFileSync(svgPath, output);
